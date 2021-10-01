97299e8463898c5fc403ecf2a8cf288ec0971c56
itspro-dev <84543009+itspro-dev@users.noreply.github.com>
2021-10-02 05:27:52 +0600

    Update callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -46,11 +46,6 @@ async def cbstart(_, query: CallbackQuery):
                         "📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}"
                     ),
                 ],
-                [
-                    InlineKeyboardButton(
-                        "🌐 Source Code", url="https://github.com/levina-lab/VeezMusic"
-                    )
-                ],
             ]
         ),
         disable_web_page_preview=True,
